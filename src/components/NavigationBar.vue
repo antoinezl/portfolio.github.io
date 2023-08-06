94dc0e1f5387133ca635815f2edbce31b5b3a219
Antoine Lu <lu.zhao.antoine@gmail.com>
2023-08-06 19:38:55 +0200

    init portfolio

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-container class="navBar">
+    <v-row class="text-center">
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <v-row justify="center">
+          <router-link to="/resume" custom v-slot="{ navigate }">
+            <span @click="navigate" role="link">resume</span>
+          </router-link>
+
+          <a
+            v-for="(next, i) in menuLinks"
+            :key="i"
+            :href="next.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ next.text }}
+          </a>
+
+          <router-link to="/about" custom v-slot="{ navigate }">
+            <span @click="navigate" role="link">a bit more about me</span>
+          </router-link>
+
+        </v-row>
+      </v-col>
+
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'navigationBar',
+
+  data () {
+    return {
+      menuLinks: [
+        {
+          text: 'linkedIn',
+          href: 'https://www.linkedin.com/in/lu-antoine/'
+        },
+        {
+          text: 'github',
+          href: 'https://github.com/antoinezl'
+        }
+      ],
+      buttons: [{
+        name: 'a bit more about me'
+      }]
+    }
+  }
+})
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <IntroHeader />
+  <PolaroidFrame />
+  <NavBar />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+// Components
+import IntroHeader from '../components/IntroHeader.vue'
+import PolaroidFrame from '../components/PolaroidFrame.vue'
+import NavBar from '../components/NavigationBar.vue'
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    IntroHeader,
+    PolaroidFrame,
+    NavBar
+  }
+})
+
+</script>
